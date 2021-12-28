ce54b30cf582f0030b117288be3c111712be692b
ForSureN1 <standin1532@gmail.com>
2021-12-28 19:00:45 +0600

    about

--- js/script.js
+++ js/script.js
@@ -31,10 +31,61 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('resize', autoPaddingHeader)
     if (header_bg) {
         function autoPaddingHeader() {
-            header_bg.style.height = document.querySelector('.header').offsetHeight + 'px'
+            header_bg.style.paddingTop = document.querySelector('.header').offsetHeight + 'px'
         }
         autoPaddingHeader();
     }
 
+
+    //Add Class Header Before Scrolling
+
+    window.addEventListener('scroll', addClassHeader)
+
+    let header = document.querySelector('.header')
+
+    function addClassHeader() {
+        if (window.pageYOffset > header.offsetHeight / 2) {
+            header.classList.add('active')
+        } else {
+            header.classList.remove('active')
+        }
+    }
+
+    addClassHeader();
+
+    //Tabs internet
+    let listinner = document.querySelector('.internet__tabs')
+
+    if (listinner) {
+        productTabs();
+        productItemTabs();
+    }
+
+    function productTabs() {
+        let btn = document.querySelectorAll('.internet__tab');
+        let block = document.querySelectorAll('.internet__content-item')
+        btn.forEach((key, index, array) => {
+            key.addEventListener('click', function() {
+                block.forEach((item, itemindex) => {
+                    item.classList.toggle('active', index === itemindex)
+                })
+            })
+        })
+    }
+
+    function productItemTabs() {
+        let list = document.querySelector('.internet__tabs')
+        let items = document.querySelectorAll('.internet__tab')
+        list.addEventListener('click', (e) => {
+            const target = e.target
+            if (target.classList.contains('internet__tab')) {
+                // console.log(target)
+                items.forEach(item => {
+                    item.classList.remove('active')
+                })
+            }
+            target.classList.add('active')
+        })
+    }
 
 })
